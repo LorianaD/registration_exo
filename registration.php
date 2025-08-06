bd4df2c399f14425bc93fdacfaa9030b1187a72e
LorianaD <loriana.diano@gmail.com>
2025-08-06 14:22:43 +0200

    add : algo validation des donnees

--- registration.php
+++ registration.php
@@ -1,15 +1,48 @@
 <?php
     //condition pour vérifier si on a recu une request en post (formulaire)
-    
-    
-    session_start();
+
+    $errors = [];
 
     if ($_SERVER["REQUEST_METHOD"] === "POST") {
-        $name = htmlspecialchars(trim("name")) ?? "";
-        $email = htmlspecialchars(trim("email")) ??"";
-        $password = htmlspecialchars(trim("password")) ?? "";
-        $confirmPassword = htmlspecialchars(trim("confirmPassword")) ?? "";
 
+        $name = htmlspecialchars(trim($_POST["name"])) ?? "";
+        $email = htmlspecialchars(trim($_POST["email"])) ??"";
+        $password = htmlspecialchars(trim($_POST["password"])) ?? "";
+        $confirmPassword = htmlspecialchars(trim($_POST["confirmPassword"])) ?? "";
+        
+        // validation des données
+        // validation username
+        // valide que le champ soit remplis
+
+        if (empty($name)) {
+            $errors[] = "nom obligatoire !";
+        //valide avec la function strlen si la string est de plus de 3 carac
+        }elseif (strlen($name) < 3) {
+            $errors[] = "mini 3 carac";
+        //valide avec la function strlen si la string est de moins de 55 carac
+        }elseif (strlen($name) > 55) {
+            $errors[] = "max 55 carac";
+        }
+
+        //validation email
+        if (empty($email)) {
+            $errors[] = "email obligatoire !";
+        } elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            $errors[] = "votre adresse ne correspond au format mail classique";
+        }
+
+        //validation password
+        if (empty($password)) { // si password vide
+            $errors[] = "password obligatoire"; // alors envoie le msg
+        }elseif (strlen($password) < 6) { // sinon si password inferieur de 6 caractéres
+            $errors[] = "password trop juste"; // alors envoie le msg
+            // normalement ici on met un pattern pour le mdp
+        }elseif ($password !== $confirmPassword) { // si password strictement différent de Confirmation de password
+            $errors[] = "mot de passe doivent etre identique"; // alors envoie le msg
+        }
+
+        var_dump($errors);
+        
     }
 ?>
 
